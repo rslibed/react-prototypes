2352fd440e52f7876e75167b984d15e348102367
rslibed <rslibed@gmail.com>
2017-12-06 18:10:01 -0800

    Feature set 2: Utilized basic JS and JSX to create output on page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const user = {
+  firstName: "Ryan",
+  lastName: "Libed",
+  luckyNumber: luckyNumber()
+};
+
+const {firstName, lastName} = user;
+
+const greeting = userObj => {
+    const divContainer = <div className={'container'}>
+        <h1>Hello there, {userObj.firstName} {userObj.lastName}!</h1>
+        <h2 className={'text-muted'}>Your lucky number is: {userObj.luckyNumber}</h2>
+    </div>
+    return divContainer;
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
