3ba3fc9f034e20ac8f6f368eb3226b96a4b0aef5
rslibed <rslibed@gmail.com>
2017-12-12 15:44:15 -0800

    Feature set2: Imported data from contact.js file into contact list using formatting from contactcard.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,11 @@
-
+import React from 'react';
+import ContactList from './contact_list';
+function App () {
+    return (<div className="container">
+        <h1 className="text-center my-3">Address Book</h1>
+            <ContactList/>
+    </div>
+    );
+}
+export default App;
 
---
+++ src/data/contact.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        firstName: 'Illidan',
+        lastName: 'Stormrage',
+        phone: '(523)569-7485',
+        email: 'evilguy528@wow.com'
+    },
+    {
+        firstName: 'Tirion',
+        lastName: 'Fordring',
+        phone: '(856)458-7412',
+        email: 'requireassistance@ashbringer.com',
+    },
+    {
+        firstName: 'Garrosh',
+        lastName: 'Hellscream',
+        phone: '1(800)I-Will-Crush-You',
+        email: 'fieryaxe4life@2drop.com'
+    },
+    {
+        firstName: 'Anduin',
+        lastName: 'Wrynn',
+        phone: '(408)777-7777',
+        email: 'bythelight@prieststilltrash.com'
+    },
+];
