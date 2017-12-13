a486a5fe1df9e90e12be1496a8dbf0da8b80c350
rslibed <rslibed@gmail.com>
2017-12-13 10:02:21 -0800

    Feature set 3: added styles to contact cards

--- src/components/contact_card.js
+++ src/components/contact_card.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
 export default props => {
-    return <h1>Full Name: {props.firstName} {props.lastName}</h1>
+    const {firstName, lastName, phone, email} = props.contact;
+    return (
+        <div className="col-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    {lastName}
+                </div>
+                <div className="card-block">
+                    <h4 className="card-title">
+                        {firstName} {lastName}
+                    </h4>
+                    <div className="card-text">
+                        <p><b>Phone: </b>{phone}</p>
+                        <p><b>Email: </b>{email}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/contact_list.js
+++ src/components/contact_list.js
@@ -11,9 +11,13 @@ class ContactList extends Component {
     }
     render () {
         const list = this.state.contacts.map( (item,index) => {
-            return <ContactCard key={index} firstName={item.firstName} lastName={item.lastName}/>;
+            return <ContactCard key={index} contact={item}/>;
         });
-        return <div>{list}</div>;
+        return (
+            <div className="col-8">
+                <div className="row">{list}</div>
+            </div>
+    )
     }
 }
 
