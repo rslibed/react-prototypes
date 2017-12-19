5d16ae904e66e72f5d36465ac1160ab163ccdad1
rslibed <rslibed@gmail.com>
2017-12-19 09:47:17 -0800

    Feature set 1,created App component and imported to index file

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,11 @@
+import React from 'react';
 
+function App () {
+    return(
+        <div className="container">
+            <h1>Here is some display text.</h1>
+        </div>
+    );
+}
 
+export default App;
