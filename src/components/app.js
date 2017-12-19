3469c5e2dcfe1ef471f540a3e655515b29df3cf6
rslibed <rslibed@gmail.com>
2017-12-19 09:57:03 -0800

    Feature set 2: created welcome component. Imported route and used Route component for welcome component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Welcome from './welcome';
 
 function App () {
     return(
         <div className="container">
-            <h1>Here is some display text.</h1>
+            <h1>React Router Prototype</h1>
+            <Route path="/" component={Welcome}/>
         </div>
     );
 }
---
+++ src/components/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome () {
+    return(
+        <div>
+            <h1>Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad architecto cupiditate deleniti, ex facilis fugit incidunt itaque iusto obcaecati odio quaerat reprehenderit voluptatibus! Aliquam doloremque qui quisquam repellendus! Accusantium aliquid commodi culpa dolores explicabo porro quasi quo. Laudantium perferendis, quaerat.</p>
+        </div>
+    );
+}
+
+export default Welcome;
