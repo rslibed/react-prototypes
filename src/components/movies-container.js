0e3c8c1921148e68f0e64008f866f727fddfb298
rslibed <rslibed@gmail.com>
2017-12-20 16:45:02 -0800

    Feature set 1: Initial App Setup

---
+++ src/components/movies-container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Movie from './movies';
+
+export default () => {
+    return(
+        <div>
+            <h2>Movie Container</h2>
+            <Movie/>
+        </div>
+    );
+}
